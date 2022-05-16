fdb5d7cc49c1eb9109b43ff2e9665dbf5c2e96cc
Jakub Podsiadły <kubapodsiadly@gmail.com>
2022-05-16 23:18:14 +0200

    Added buttons and Equation

--- Zadanie_4/script.js
+++ Zadanie_4/script.js
@@ -35,3 +35,68 @@ navbarToggler.addEventListener('click', () => {
     else
         toToggle.classList.add('show');
 })
+
+let degree = 0;
+let equations = [];
+let coefficients = [];
+equations[0] = '<input class="equation-coefficient">';
+
+const increaseXButton = document.querySelector('#increaseX');
+increaseXButton.addEventListener('click', () => {
+    let equation = document.querySelector('.equation')
+
+    // console.log("Incerase" + degree);
+    if (degree >= 11) {
+        return;
+    }
+    if (degree === 0) {
+        equation.innerHTML = '<input class="equation-coefficient ' + "d" + degree + ' ">';
+    } else if (degree === 1) {
+        equation.innerHTML = '<input class="equation-coefficient ' + "d" + degree + ' ">' +
+            '<p class="equation-static"> X + </p>'
+            + equation.innerHTML;
+    } else {
+        equation.innerHTML = '<input class="equation-coefficient ' + "d" + degree + ' ">' +
+            '<p class="equation-static"> X^ ' + degree + ' + </p>'
+            + equation.innerHTML;
+    }
+    equations[degree] = equation.innerHTML
+    degree++;
+})
+
+const decreaseXButton = document.querySelector('#decreaseX');
+decreaseXButton.addEventListener('click', () => {
+    let equation = document.querySelector('.equation');
+    // let test = document.querySelector('.d'+degree);
+
+    // console.log(test.className);
+    // console.log("Decerase" + degree);
+    if (degree < 1) {
+        equation.innerHTML = equations[0];
+    } else {
+        degree--;
+        equation.innerHTML = equations[degree];
+    }
+})
+
+
+// var answers = 0,
+//     write = document.getElementById('buttons');
+//
+// function addAnswer() {
+//     write.innerHTML += 'Add answer: <input type="text" id="answer"' + answers + '/> <br />';
+//     answers++;
+// }
+
+
+// var i = 0;
+//
+// function addMore()
+// {
+//     var x = document.getElementById('buttons');
+//     var input1 = document.createElement("input");
+//     input1.setAttribute("type","text");
+//     input1.setAttribute("name","i" + i );
+//     x.appendChild( input1 );
+//     i++;
+// }
